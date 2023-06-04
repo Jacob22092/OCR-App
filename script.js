d47e61c38d10442767d26e9f5d1b1516980fb090
Jakub Hyziak <jakub2001411@gmail.com>
2023-06-04 17:46:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const darkmodeToggle = document.getElementById("darkmode-toggle");
   const body = document.body;
   const container = document.querySelector(".container");
+  const processingOverlay = document.getElementById("processing-overlay");
 
   fileUpload.addEventListener("change", () => {
     fileUploadLabelText.innerText = fileUpload.files[0].name;
   });
 
   submitButton.addEventListener("click", () => {
+    processingOverlay.style.display = "flex"; // Pokaż overlay i animację
     performOCR(fileUpload.files[0]);
   });
 
@@ -28,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
       { logger: m => console.log(m) }
     ).then(({ data: { text } }) => {
       outputTextArea.value = text;
+      processingOverlay.style.display = "none"; // Ukryj overlay i animację po zakończeniu przetwarzania
     });
   };
 });
